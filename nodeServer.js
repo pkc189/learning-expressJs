8701565aacf43f3792efd7b383b58df490fd5220
pankaj-techment <121149684+pankaj-techment@users.noreply.github.com>
2023-05-13 17:34:10 +0530

    nosw server

--- nodeServer.js
+++ nodeServer.js
@@ -1,8 +1,13 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>hello</h1");
+  if (req.url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>hello</h1");
+  } else {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>not found</h1");
+  }
   res.end();
 });
 
